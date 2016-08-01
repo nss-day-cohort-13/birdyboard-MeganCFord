abd9b2b85f5c5ed4870e1fb8b31348d5b30f001c
Megan Ford <meg.c.ford@gmail.com>
2016-08-01 15:00:19 -0500

    add new public tweet thread function complete. moved some inputs out of printers

--- birdyboard.py
+++ birdyboard.py
@@ -7,7 +7,7 @@ class Birdyboard:
 
     def __init__(self):
         self.user_name = "Megan"  # later I will set this to None and have the login menu reassign it.
-        self.public_chirps = []
+        self.chirps_library = {}
         self.chirp_to_display = None
 
     # TODO: add a decorator here that will print a title heading so I can use it for both the logged in menu and the unlogged in menu?
@@ -16,7 +16,7 @@ class Birdyboard:
         prints the choices for an unlogged-in user, at start of app. calls the unlogged_in_menu_next_step method to determine where to go next.
         Arguments: none
         """
-        print("1. view chirps\n2. new chirp (public)\n'x' to exit.")
+        print("1. new user\n2. log in\n3. view public chirps\n'x' to exit.")
 
     def unlogged_in_menu_next_step(self):
         """
@@ -28,28 +28,27 @@ class Birdyboard:
         if len(next_step) > 1:
             print("you only need to type one character!")
             self.unlogged_in_menu_next_step()
-        elif next_step not in ["1", "2", "x"]:
+        elif next_step not in ["1", "2", "3", "4", "x"]:
             print("please choose one of the options shown above.")
             self.unlogged_in_menu_next_step()
-        elif next_step == "1":
-            self.unlogged_in_view_chirps()
+        elif next_step == "3":
+            self.deserialize_chirps_library()
+            self.view_chirps()
             self.view_chirps_next_step()
-        elif next_step == "2":
+        elif next_step == "4":
             # TODO: remove this from options, I don't want people chirping without true login.
-            self.unlogged_in_new_chirp()
+            self.new_chirp_menu()
             # TODO: handle logging in.
         elif next_step == "x":
             print("goodbye.")
             exit()
 
-    def unlogged_in_view_chirps(self):
-        # load the 'chirps' .txt file.
-        self.all_chirps = self.deserialize_chirps_library()
-        # assign the public part of the .txt file to an object.
-        self.public_chirps = self.all_chirps["public"]
-        # print the first chirp in the public list for each public thread.
-        # formatted.
-        [print(str(self.public_chirps.index(chirp) + 1) + ". " + chirp[0][0] + ": " + chirp[0][1]) for chirp in self.public_chirps]
+    def view_chirps(self):
+        print("******ALL CHIRPS******")
+        if len(self.chirps_library["public"]) >0:
+            [print(str(self.chirps_library["public"].index(chirp) + 1) + ". " + chirp[0][0] + ": " + chirp[0][1]) for chirp in self.chirps_library["public"]]
+        else:
+            print("no chirps yet!")
 
     def view_chirps_next_step(self):
         next_step = input("enter the number of the chirp you'd like to view the full thread for.\n'x' to go back.\n>> ")
@@ -57,6 +56,7 @@ class Birdyboard:
         if next_step == "x":
             print("going back.")
             self.unlogged_in_menu_print()
+            self.unlogged_in_menu_next_step()
         else:
             try:
                 next_step == int(next_step)
@@ -66,9 +66,10 @@ class Birdyboard:
             finally:
                 try:
                     if int(next_step)-1 >= 0:
+                        # TODO: grab whether or not this is a public or private one.
                         chirp_index = int(next_step)-1
-                        self.view_full_chirp(chirp_index)
-                        self.go_back_from_tweet_thread()
+                        self.view_full_chirp("public", chirp_index)
+                        self.chirp_thread_menu()
                     else:
                         print("your number is not in the list of chirps.")
                         self.view_chirps_next_step()
@@ -78,37 +79,56 @@ class Birdyboard:
                 finally:
                     pass
 
-    def view_full_chirp(self, chirp_index):
-        self.chirp_to_display = self.public_chirps[chirp_index]
-        [print(chirp[0] + ": " + chirp[1]) for chirp in self.chirp_to_display]
+    def view_full_chirp(self, public_or_private, chirp_index):
+        self.chirp_to_display = {"location": public_or_private, "index": chirp_index, "chirp list": self.chirps_library[public_or_private][chirp_index]}
+        [print(chirp[0] + ": " + chirp[1]) for chirp in self.chirp_to_display["chirp list"]]
 
-    def go_back_from_tweet_thread(self):
+    def chirp_thread_menu(self):
+        # TODO: add an 'add comment' option here.
         next_step = input("'x' to go back.\n>> ")
 
         if next_step == "x":
             print("going back.")
             self.chirp_to_display = None
-            self.unlogged_in_view_chirps()
+            self.view_chirps()
+            self.view_chirps_next_step()
         else:
             print("command not recognized.")
             self.go_back_from_tweet_thread()
+
+
+    def new_chirp_menu(self):
+        # TODO: handle private chirps.
+        if self.chirp_to_display is not None:
+            print("Add Chirp to thread:" + self.user_name)
+        else:
+            print("New Chirp Thread: " + self.user_name)
+        chirp_to_add = input("chirp text: >> ")
+        # add this chirp onto the public chirps list. deserialize if already not- you can add a new chirp without having viewed them, so just in case.
+        self.deserialize_chirps_library()
+        self.chirps_library["public"].append([(self.user_name, chirp_to_add)])
+        self.serialize_chirps_library()
+        self.view_chirps()
+        self.view_chirps_next_step()
+
+    def serialize_chirps_library(self):
+        with open("chirps.txt", "wb+") as chirps:
+            pickle.dump(self.chirps_library, chirps)
 
     def deserialize_chirps_library(self):
         # TODO: change default library to reflect the lack of messages in a more meaningful way. these are just test messages.
         # TODO: add a 'delete thread' option so I can get rid of these default messages?
         try:
             with open('chirps.txt', 'rb') as chirps:
-                chirp_library = pickle.load(chirps)
+                self.chirps_library = pickle.load(chirps)
 
         except FileNotFoundError:
-                chirp_library = {"public": [[("username", "sample_text")]]}
+                self.chirps_library = {"public": [], "private": []}
         except EOFError:
-                chirp_library = {"public": [[("username", "sample text"), ("another_username", "more sample text.")], [("username", "second thread"), ("username", "second thread second message.")]]}
-        return chirp_library
+                self.chirps_library = {"public": [], "private": []}
 
 
 if __name__ == '__main__':
     app = Birdyboard()
     app.unlogged_in_menu_print()
-    # I've separated out the input so I can re-run it alone for easier error handling.
     app.unlogged_in_menu_next_step()
