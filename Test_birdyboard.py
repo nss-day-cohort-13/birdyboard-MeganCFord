d21ff69b2eb7de0b85ed6676aa0945cf4c66a490
Megan Ford <meg.c.ford@gmail.com>
2016-08-01 13:16:26 -0500

    moved functions within menus instead of chaining them. wrote test to check that chirp_to_display assigns correctly.

--- Test_birdyboard.py
+++ Test_birdyboard.py
@@ -18,7 +18,13 @@ class Test_Birdyboard(unittest.TestCase):
         self.assertIsInstance(self.test_app.public_chirps, list)
         self.assertEqual(len(self.test_app.public_chirps), 0)
         self.test_app.unlogged_in_view_chirps()
-        # test that length is no longer 0 I guess?
+        self.assertNotEqual(len(self.test_app.public_chirps), 0)
+
+    def test_view_full_public_tweet(self):
+        self.public_tweet_test = Birdyboard()
+        self.public_tweet_test.public_chirps = [[("hey", "what"), ("no", "thank you")], [("second", "text"), ("not", "sure how this will work.")]]
+        self.public_tweet_test.view_full_chirp(0)
+        self.assertEqual(self.public_tweet_test.chirp_to_display, [("hey", "what"), ("no", "thank you")])
 
 if __name__ == '__main__':
     unittest.main()
--- birdyboard.py
+++ birdyboard.py
@@ -7,7 +7,8 @@ class Birdyboard:
 
     def __init__(self):
         self.user_name = "Megan"  # later I will set this to None and have the login menu reassign it.
-        self.public_chirps = list
+        self.public_chirps = []
+        self.chirp_to_display = None
 
     # TODO: add a decorator here that will print a title heading so I can use it for both the logged in menu and the unlogged in menu?
     def unlogged_in_menu_print(self):
@@ -15,9 +16,7 @@ class Birdyboard:
         prints the choices for an unlogged-in user, at start of app. calls the unlogged_in_menu_next_step method to determine where to go next.
         Arguments: none
         """
-        print("1. view chirps\n2. new chirp (public)")
-        #  I've separated out the input so I can re-run it alone for easier error handling.
-        self.unlogged_in_menu_next_step()
+        print("1. view chirps\n2. new chirp (public)\n'x' to exit.")
 
     def unlogged_in_menu_next_step(self):
         """
@@ -25,17 +24,23 @@ class Birdyboard:
         Arguments: none
         """
         next_step = input(">> ")
+
         if len(next_step) > 1:
             print("you only need to type one character!")
             self.unlogged_in_menu_next_step()
-        elif next_step not in ["1", "2"]:
+        elif next_step not in ["1", "2", "x"]:
             print("please choose one of the options shown above.")
             self.unlogged_in_menu_next_step()
         elif next_step == "1":
             self.unlogged_in_view_chirps()
+            self.view_chirps_next_step()
         elif next_step == "2":
             # TODO: remove this from options, I don't want people chirping without true login.
             self.unlogged_in_new_chirp()
+            # TODO: handle logging in.
+        elif next_step == "x":
+            print("goodbye.")
+            exit()
 
     def unlogged_in_view_chirps(self):
         # load the 'chirps' .txt file.
@@ -45,9 +50,6 @@ class Birdyboard:
         # print the first chirp in the public list for each public thread.
         # formatted.
         [print(str(self.public_chirps.index(chirp) + 1) + ". " + chirp[0][0] + ": " + chirp[0][1]) for chirp in self.public_chirps]
-        self.view_chirps_next_step()
-        # assign some kind of value to them, so that the user can access the full list.
-        # go back option sends back to unlogged in menu print.
 
     def view_chirps_next_step(self):
         next_step = input("enter the number of the chirp you'd like to view the full thread for.\n'x' to go back.\n>> ")
@@ -58,23 +60,38 @@ class Birdyboard:
         else:
             try:
                 next_step == int(next_step)
+            except ValueError:
+                print("you didn't enter a number.")
+                self.view_chirps_next_step()
+            finally:
                 try:
-                    chirp_to_show = self.public_chirps[int(next_step)-1]
-                    print(chirp_to_show)
-                    self.view_full_chirp(chirp_to_show)
+                    if int(next_step)-1 >= 0:
+                        chirp_index = int(next_step)-1
+                        self.view_full_chirp(chirp_index)
+                        self.go_back_from_tweet_thread()
+                    else:
+                        print("your number is not in the list of chirps.")
+                        self.view_chirps_next_step()
                 except IndexError:
                     print("your number is not in the list of chirps.")
                     self.view_chirps_next_step()
                 finally:
                     pass
-            except TypeError:
-                print("you didn't enter a number.")
-                self.view_chirps_next_step()
-            finally:
-                pass
 
-    def view_full_chirp(self, chirp_to_show):
-        print(chirp_to_show)
+    def view_full_chirp(self, chirp_index):
+        self.chirp_to_display = self.public_chirps[chirp_index]
+        [print(chirp[0] + ": " + chirp[1]) for chirp in self.chirp_to_display]
+
+    def go_back_from_tweet_thread(self):
+        next_step = input("'x' to go back.\n>> ")
+
+        if next_step == "x":
+            print("going back.")
+            self.chirp_to_display = None
+            self.unlogged_in_view_chirps()
+        else:
+            print("command not recognized.")
+            self.go_back_from_tweet_thread()
 
     def deserialize_chirps_library(self):
         # TODO: change default library to reflect the lack of messages in a more meaningful way. these are just test messages.
@@ -89,12 +106,9 @@ class Birdyboard:
                 chirp_library = {"public": [[("username", "sample text"), ("another_username", "more sample text.")], [("username", "second thread"), ("username", "second thread second message.")]]}
         return chirp_library
 
-    def unlogged_in_view_full_tweet(self):
-        pass
-        # grab the value of the thread that the user chose. Do I want to assign it a value in the .txt, like as a dictionary?
-        # print all the messages in the thread.
-        # do not allow for new tweets- simply a go back option which sends back to view chirps.
 
 if __name__ == '__main__':
     app = Birdyboard()
     app.unlogged_in_menu_print()
+    # I've separated out the input so I can re-run it alone for easier error handling.
+    app.unlogged_in_menu_next_step()
