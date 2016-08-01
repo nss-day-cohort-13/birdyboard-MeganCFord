7a0d84c3628029b12b7395ba9484d379c2541549
Megan Ford <meg.c.ford@gmail.com>
2016-08-01 16:06:45 -0500

    added some additional ux options. wrote docstrings and made sure existing tests pass.

--- Test_birdyboard.py
+++ Test_birdyboard.py
@@ -8,21 +8,29 @@ class Test_Birdyboard(unittest.TestCase):
     def setUpClass(self):
         self.test_app = Birdyboard()
 
-    def test_username_default(self):
+    def test_defaults(self):
         # TODO: eventually change this to test that default username equals None.
         self.assertEqual(self.test_app.user_name, "Megan")
 
     def test_that_public_tweets_load_correctly(self):
-        self.assertIsInstance(self.test_app.public_chirps, list)
-        self.assertEqual(len(self.test_app.public_chirps), 0)
-        self.test_app.unlogged_in_view_chirps()
-        self.assertNotEqual(len(self.test_app.public_chirps), 0)
+        self.assertIsInstance(self.test_app.chirps_library, dict)
+        self.assertEqual(len(self.test_app.chirps_library), 2)
+        self.test_app.deserialize_chirps_library()
+        self.assertEqual(len(self.test_app.chirps_library), 2)
+        self.assertNotEqual(len(self.test_app.chirps_library["public"]), 0)
 
     def test_view_full_public_tweet(self):
         self.public_tweet_test = Birdyboard()
-        self.public_tweet_test.public_chirps = [[("hey", "what"), ("no", "thank you")], [("second", "text"), ("not", "sure how this will work.")]]
-        self.public_tweet_test.view_full_chirp(0)
-        self.assertEqual(self.public_tweet_test.chirp_to_display, [("hey", "what"), ("no", "thank you")])
+        self.public_tweet_test.chirps_library = {"public": [[("hey", "what"), ("no", "thank you")], [("second", "text"), ("not", "sure how this will work.")]], "private": []}
+        self.public_tweet_test.view_full_chirp("public", 0)
+        self.assertEqual(self.public_tweet_test.current_chirp, {"location": "public", "index": 0, "chirps": [("hey", "what"), ("no", "thank you")]})
+
+    def test_that_public_tweet_adds(self):
+        pass
+
+    def test_that_you_can_add_to_a_public_tweet(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
--- birdyboard.py
+++ birdyboard.py
@@ -17,20 +17,20 @@ class Birdyboard:
 
     def __init__(self):
         self.user_name = "Megan"  # later I will set this to None and have the login menu reassign it.
-        self.chirps_library = {}
-        self.chirp_to_display = None
+        self.chirps_library = {"public": [], "private": []}
+        self.current_chirp= None
 
     # TODO: add a decorator here that will print a title heading so I can use it for both the logged in menu and the unlogged in menu?
     def unlogged_in_menu_print(self):
         """
-        prints the choices for an unlogged-in user, at start of app. calls the unlogged_in_menu_next_step method to determine where to go next.
+        prints the choices for an unlogged-in user, at start of app.
         Arguments: none
         """
         print("1. new user\n2. log in\n3. view public chirps\n'x' to exit.")
 
     def unlogged_in_menu_next_step(self):
         """
-        requests input from an unlogged-in user as part of the unlogged-in menu. looking for one of the options printed in the unlogged_in_menu_print.
+        requests input for top level menu functionality while the user is not logged in. Currently does not do anything for user login. secret fourth option to add a chirp but later that will be only an option if you're logged in.
         Arguments: none
         """
         next_step = input(">> ")
@@ -48,27 +48,46 @@ class Birdyboard:
         elif next_step == "4":
             # TODO: remove this from options, I don't want people chirping without true login.
             self.new_chirp_menu()
-            # TODO: handle logging in.
         elif next_step == "x":
             print("goodbye.")
             exit()
+            # TODO: handle these two.
+        elif next_step == "1":
+            print('create a user page will show up here.')
+            self.unlogged_in_menu_next_step()
+        elif next_step == "2":
+            print('log in to a current user will show up here.')
+            self.unlogged_in_menu_next_step()
 
     def view_chirps(self):
+        """
+        this function is called if the user presses "3" on the unlogged-in menu, after deserialization. prints the first chirp of all public chirp threads, formatted to also show the user name, and print a readable index number for selection of 'view full chirp'.
+        Arguments: none.
+        """
         print("******ALL CHIRPS******")
         if len(self.chirps_library["public"]) > 0:
             print("PUBLIC:")
             [print(str(self.chirps_library["public"].index(chirp) + 1) + ". " + chirp[0][0] + ": " + chirp[0][1]) for chirp in self.chirps_library["public"]]
+            print("\n")
         else:
             print("no public chirps yet!")
+            print("\n")
         # TODO: print the private chirps.
 
     def view_chirps_next_step(self):
-        next_step = input("enter the number of the chirp you'd like to view the full thread for.\n'x' to go back.\n>> ")
+        """
+        menu that appears after all chirps are printed (view_chirps). Allows the user to view a full chirp thread based on the index, or to go back or exit.
+        arguments: none
+        """
+        next_step = input("enter the number of the chirp you'd like to view the full thread for.\n'b' to go back.\n'x' to exit.>> ")
 
-        if next_step == "x":
+        if next_step == "b":
             print("going back.")
             self.unlogged_in_menu_print()
             self.unlogged_in_menu_next_step()
+        elif next_step == "x":
+            print("goodbye.")
+            exit()
         else:
             try:
                 next_step == int(next_step)
@@ -78,8 +97,8 @@ class Birdyboard:
             finally:
                 try:
                     if int(next_step)-1 >= 0:
-                        # TODO: grab whether or not this is a public or private one.
                         chirp_index = int(next_step)-1
+                        # TODO: grab whether or not this is a public or private one and determine the correct private-tweet index.
                         self.view_full_chirp("public", chirp_index)
                         self.chirp_thread_menu()
                     else:
@@ -92,57 +111,90 @@ class Birdyboard:
                     pass
 
     def view_full_chirp(self, public_or_private, chirp_index):
-        self.chirp_to_display = {"location": public_or_private, "index": chirp_index, "chirp list": self.chirps_library[public_or_private][chirp_index]}
-        [print(chirp[0] + ": " + chirp[1]) for chirp in self.chirp_to_display["chirp list"]]
-        self.new_chirp_menu()
+        """
+        displays a full chirp based on the index chosen in view_chirps_next_step. currently only handles public tweets. The numerical display of the public tweets makes sense, but grabbing the private tweets by index isn't going to work so I'm going to need to come up with another way to do that.
+        Arguments: 1. string "public" or string "private" for location, 2. index number.
+        """
+        self.current_chirp = {"location": public_or_private, "index": chirp_index, "chirps": self.chirps_library[public_or_private][chirp_index]}
+        [print(chirp[0] + ": " + chirp[1]) for chirp in self.current_chirp["chirps"]]
+        print("\n")
 
     def chirp_thread_menu(self):
+        """
+        prints after view_full_chirp to ask whether the user would like to go back, exit, or add to the chirp thread.
+        arguments: none
+        """
         # TODO: add an 'add comment' option here.
-        next_step = input("'x' to go back.\n>> ")
+        next_step = input("1. add a new chirp to this thread.\n'b' to go back.\n'x' to exit.\n>> ")
 
-        if next_step == "x":
+        if next_step not in["1", "x", "b"]:
+            print("command not recognized.")
+            self.chirp_thread_menu()
+        elif next_step == "b":
             print("going back.")
             self.chirp_to_display = None
             self.view_chirps()
             self.view_chirps_next_step()
-        else:
-            print("command not recognized.")
-            self.go_back_from_tweet_thread()
+        elif next_step == "1":
+            self.new_chirp_menu()
+        elif next_step == "b":
+            print("goodbye.")
+            exit()
 
 
     def new_chirp_menu(self):
+        """
+        UNFINISHED, will probably break out at a higher level. Currently, runs when '1' is chosen in chirp_thread_menu, or when option 4 is chosen in top level menu to start a new thread. either adds a completely new chirp thread with one item in it, or appends a chirp to an existing thread.
+        Arguments: none
+        """
         # TODO: handle private chirps.
-        if self.chirp_to_display is not None:
+        if self.current_chirp is not None:
 
-            if self.chirp_to_display["location"] == "public":
+            if self.current_chirp["location"] == "public":
                 # TODO: nest the adding chirp into a further menu so you can view a thread without having to add to it.
-                print("Add Chirp to public thread:" + self.user_name)
-                chirp_to_add = input("chirp text: >> ")
-                self.chirps_library["public"][self.chirp_to_display["index"]].append((self.user_name, chirp_to_add))
-                self.serialize_chirps_library()
-                self.chirp_to_display = None
-                self.view_chirps()
-                self.view_chirps_next_step()
-            elif self.chirp_to_display["location"] == "private":
+                chirp_to_add = input("'b' to go back.\n'x' to exit.\nchirp text: >> ")
+                if chirp_to_add == "b":
+                    print("going back.")
+                elif chirp_to_add == "x":
+                    print("goodbye.")
+                    exit()
+                else:
+                    self.chirps_library["public"][self.current_chirp["index"]].append((self.user_name, chirp_to_add))
+                    self.serialize_chirps_library()
+                    self.chirp_to_display = None
+                self.view_full_chirp(self.current_chirp["location"], self.current_chirp["index"])
+                self.chirp_thread_menu()
+                # TODO: handle private tweeting.
+            elif self.current_chirp["location"] == "private":
                 print("private chirps will go here.")
                 self.chirp_thread_menu()
         else:
-            print("New Chirp Thread: " + self.user_name)
-        chirp_to_add = input("chirp text: >> ")
-        # add this chirp onto the public chirps list. deserialize if already not- you can add a new chirp without having viewed them, so just in case.
-        self.deserialize_chirps_library()
-        self.chirps_library["public"].append([(self.user_name, chirp_to_add)])
-        self.serialize_chirps_library()
+            print("'b' to go back.\n'x' to exit.\n******NEW CHIRP THREAD:******")
+            chirp_to_add = input("\nchirp text: >> ")
+            if chirp_to_add == "b":
+                print("going back.")
+            elif chirp_to_add == "x":
+                print("goodbye.")
+                exit()
+            else:
+                self.deserialize_chirps_library()
+                self.chirps_library["public"].append([(self.user_name, chirp_to_add)])
+                self.serialize_chirps_library()
         self.view_chirps()
         self.view_chirps_next_step()
 
     def serialize_chirps_library(self):
+        """
+        saves new chirps to the chirps.txt file.
+        Arguments: none
+        """
         with open("chirps.txt", "wb+") as chirps:
             pickle.dump(self.chirps_library, chirps)
 
     def deserialize_chirps_library(self):
-        # TODO: change default library to reflect the lack of messages in a more meaningful way. these are just test messages.
-        # TODO: add a 'delete thread' option so I can get rid of these default messages?
+        """
+        opens chirps.txt file. handles what happens if there are no chirps.
+        """
         try:
             with open('chirps.txt', 'rb') as chirps:
                 self.chirps_library = pickle.load(chirps)
